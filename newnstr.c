9916a78f2eb6b9e71392b063954ca1ca24c78274
scs <scs>
2006-01-27 13:52:53 +0000

    Started using standard posix string include definitions.

--- newnstr.c
+++ newnstr.c
@@ -1,13 +1,16 @@
 /*
  *  Library function to create a new string.
  *
- *  $RCSfile: newnstr.c,v $	$Revision: 1.5 $
+ *  $RCSfile: newnstr.c,v $	$Revision: 1.6 $
  *
- *  $Author: scs $	$Date: 1996/09/29 01:05:49 $
+ *  $Author: scs $	$Date: 2006/01/27 13:52:53 $
  *
  *  $State: Exp $	$Locker:  $
  *
  *  $Log: newnstr.c,v $
+ *  Revision 1.6  2006/01/27 13:52:53  scs
+ *  Started using standard posix string include definitions.
+ *
  *  Revision 1.5  1996/09/29 01:05:49  scs
  *  Removed old stdc stuff, minor bug fixes for stdin-only mode.
  *
@@ -16,19 +19,19 @@
  *
  *  Revision 1.3  89/11/12  22:01:28  scs
  *  First production release.  Stripped all useless history and side-alleys.
- *  
  */
 
 #ifndef	lint
 #ifndef	lib
-static char	rcsid[] = "$Id: newnstr.c,v 1.5 1996/09/29 01:05:49 scs Exp $" ;
+static char	rcsid[] = "$Id: newnstr.c,v 1.6 2006/01/27 13:52:53 scs Exp $" ;
 #endif
 #endif
 
 # include	<stdio.h>
 # include	<stdc.h>
+# include	<string.h>
 
-extern voidptr	malloc( int ) ;
+extern void*	malloc( int ) ;
 
 /*
  *  Create a new string of a given length.  Copy the incoming string
@@ -42,6 +45,6 @@ char*	NewNString( register char* string, register unsigned len )
 
 	if ( NULL == ( new_string = malloc( len + 1 ) ) )
 		return NULL ;
-	(void) strncpy( new_string, string, (int) len ) ;
+	strncpy( new_string, (const char*) string, (size_t) len ) ;
 	return new_string ;
 }
